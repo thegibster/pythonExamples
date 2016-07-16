ea77ba5578652e6ad915027383933b7c13324f5a
andrew gibbs <andrewgibbs@outlook.com>
2016-07-16 16:03:46 -0700

    added movie trailer method

---
+++ intro python/movies/entertainment_center.py
@@ -0,0 +1,7 @@
+import media
+
+toy_story = media.Movie("Toy Story","A story of a boy and his toys.","http://goo.gl/i4rQ8Y", "https://www.youtube.com/watch?v=m_43XucTd3o")
+
+print (toy_story.storyline)
+toy_story.show_trailer()
+                    
