c4559447a787e37eae7aecef6c8b923023fc1877
andrew gibbs <andrewgibbs@outlook.com>
2016-07-16 17:07:52 -0700

    using a basic inheritance example to practice class and class inheritance

--- intro python/movies/entertainment_center.py
+++ intro python/movies/entertainment_center.py
@@ -1,7 +1,13 @@
+import fresh_tomatoes
 import media
 
 toy_story = media.Movie("Toy Story","A story of a boy and his toys.","http://goo.gl/i4rQ8Y", "https://www.youtube.com/watch?v=m_43XucTd3o")
 
-print (toy_story.storyline)
-toy_story.show_trailer()
+movies =[toy_story]
+#fresh_tomatoes.open_movies_page(movies)
+print(media.Movie.__doc__)
+#print(media.Movie.VAILD_RATINGS)
+#print (toy_story.storyline)
+#toy_story.show_trailer()
+
                     
